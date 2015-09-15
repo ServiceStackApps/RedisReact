15dfc5cfea49d0502ec8c090b5b180d29051ea3a
Demis Bellot <demis.bellot@gmail.com>
2015-09-15 06:22:20 -0400

    rename to lowercase

--- src/RedisReact/RedisReact.AppMac/Main.cs
+++ src/RedisReact/RedisReact.AppMac/Main.cs
@@ -8,17 +8,19 @@ namespace RedisReact.AppMac
 {
 	public static class MainClass
 	{
-		public static string HostUrl = "http://localhost:3337/";
+		public static string HostUrl = "http://127.0.0.1:3337/";
+		public static string ListenOn = "http://*:3337/";
+
 		public static AppHost App;
 		public static NSMenu MainMenu;
 
 		static void Main (string[] args)
 		{
-			App = new AppHost ();
-			App.Init ().Start (HostUrl);
+			App = new AppHost();
+			App.Init().Start(ListenOn);
 
-			NSApplication.Init ();
-			NSApplication.Main (args);
+			NSApplication.Init();
+			NSApplication.Main(args);
 		}
 	}
 }
