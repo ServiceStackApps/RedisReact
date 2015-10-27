f31d8a7c604bb7638323e554f61d30b8839c78f3
Demis Bellot <demis.bellot@gmail.com>
2015-10-27 10:54:23 -0400

    Switch to use consistent 2337 port

--- src/RedisReact/RedisReact.AppMac/Program.cs
+++ src/RedisReact/RedisReact.AppMac/Program.cs
@@ -9,8 +9,8 @@ namespace RedisReact.AppMac
 {
 	public static class Program
 	{
-		public static string HostUrl = "http://localhost:3337/";
-		public static string ListenOn = "http://*:3337/";
+		public static string HostUrl = "http://localhost:2337/";
+		public static string ListenOn = "http://*:2337/";
 
 		public static AppHost App;
 		public static NSMenu MainMenu;
